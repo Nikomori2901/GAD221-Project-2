b61d1550e7ccec0565b9e486a4116b511ef84ae6
Niko <nikomori2901@gmail.com>
2024-11-04 06:10:26 +0800

    Tooltips & Setup
    Imported TMP Essentials.
    Created multiple folders and empty scripts as placeholders.
    Created Script for storing Employee information.
    Created Scripts for General and Employee Tooltips to display information about interactable objects.

---
+++ Assets/UI/Tooltips/EmployeeTooltip.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmployeeTooltip : Tooltip
+{
+    [SerializeField] TMP_Text nameText;
+    [SerializeField] TMP_Text moraleText;
+    [SerializeField] TMP_Text designText;
+    [SerializeField] TMP_Text programmingText;
+    [SerializeField] TMP_Text artText;
+    [SerializeField] TMP_Text audioText;
+
+    public void SetEmployeeInfo(GameObject employeeObject)
+    {
+        Employee employee = employeeObject.GetComponent<Employee>();
+
+        nameText.text = employee.employeeName;
+        moraleText.text = "Morale: " + employee.employeeMorale;
+        designText.text = "Design: " + employee.designSkill;
+        programmingText.text = "Programming: " + employee.programmingSkill;
+        artText.text = "Art: " + employee.artSkill;
+        audioText.text = "Audio: " + employee.audioSkill;
+    }
+}
