42f5f773731a1c429a33adb91103fb7d3c0ccab3
Niko <nikomori2901@gmail.com>
2024-11-11 02:10:59 +0800

    2D & Employee Grabbing
    Minor Bug with tooltips sometimes lingering in very specific circumstances that can be fixed later.

--- Assets/Employees/Employee.cs
+++ Assets/Employees/Employee.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Employee : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class Employee : MonoBehaviour
     [SerializeField] public int artSkill;
     [SerializeField] public int audioSkill;
 
+    [SerializeField] UnityEvent<GameObject> showTooltip;
+    [SerializeField] UnityEvent<GameObject> hideTooltip;
+    [SerializeField] UnityEvent<Employee> onMouseDown;
+
+    public bool grabbed = false;
+
     public void Initialize(string name, int morale, int design, int programming, int art, int audio)
     {
         employeeName = name;
@@ -20,6 +27,27 @@ public class Employee : MonoBehaviour
         programmingSkill = programming;
         artSkill = art;
         audioSkill = audio;
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Hover");
+        showTooltip.Invoke(gameObject);
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Unhover");
+        if (!grabbed)
+        {
+            hideTooltip.Invoke(gameObject);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        grabbed = true;
+        onMouseDown.Invoke(this);
     }
 
     void Start()
--- Assets/Employees/EmployeeGrabber.cs
+++ Assets/Employees/EmployeeGrabber.cs
@@ -4,20 +4,72 @@ using UnityEngine;
 
 public class EmployeeGrabber : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Employee currentEmployee;
+    public bool currentlyHolding;
+    private Vector3 employeeOriginalPos;
+
     void Start()
     {
-        
+        InputHandler.current.onM1Up += ReleaseEmployee;
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    // current employee holding
-    // if currently holding
+    public void GrabEmployee(Employee employee)
+    {
+        if (!currentlyHolding)
+        {
+            currentEmployee = employee;
+            currentlyHolding = true;
+            employeeOriginalPos = employee.transform.position;
+            StartCoroutine(Holding());
+        }
+    }
+
+    private IEnumerator Holding()
+    {
+        while (currentlyHolding)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, currentEmployee.transform.position.z));
+            currentEmployee.transform.position = new Vector3 (mousePos.x, mousePos.y, currentEmployee.transform.position.z);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    public void ReleaseEmployee()
+    {
+        if (currentlyHolding)
+        {
+            currentEmployee.grabbed = false;
+            ReturnEmployee();
+        }
+        
+        // when released if on station that is free assigns them, if not, snaps employee back to original location
+    }
+
+    public void AssignEmployee()
+    {
+
+    }
+
+    public void ReturnEmployee()
+    {
+        currentEmployee.transform.position = employeeOriginalPos;
+        ClearEmployee();
+    }
+
+    private void ClearEmployee()
+    {
+        currentEmployee = null;
+        currentlyHolding = false;
+        employeeOriginalPos = Vector3.zero;
+        StopAllCoroutines();
+    }
+
     // grabs employee when clicking on them
-    // when released if on station that is free assigns them, if not, snaps employee back to original location
+    
 }
